23d461a47d730e735a44465857af31b30f418619
Maxime Lapointe <hunter_spawn@hotmail.com>
2021-08-30 09:39:03 -0400

    Skip some tests that are not compatible with 4.2

--- spec/instance_dependent_spec.rb
+++ spec/instance_dependent_spec.rb
@@ -6,18 +6,22 @@ require_relative "support/spec_helper"
 
 describe "follow_assoc" do
   it "refuses instance dependent has_many" do
+    skip if ActiveRecord.gem_version < Gem::Version.new("5.0")
     expect { Post.follow_assoc(:instance_dependent_comments) }.to raise_error(/instance dependent/)
   end
 
   it "refuses instance dependent belongs_to" do
+    skip if ActiveRecord.gem_version < Gem::Version.new("5.0")
     expect { Comment.follow_assoc(:instance_dependent_post) }.to raise_error(/instance dependent/)
   end
 
   it "refuses instance dependent has_one" do
+    skip if ActiveRecord.gem_version < Gem::Version.new("5.0")
     expect { Post.follow_assoc(:instance_dependent_one_comment) }.to raise_error(/instance dependent/)
   end
 
   it "refuses instance dependent has_and_belongs_to_many" do
+    skip if ActiveRecord.gem_version < Gem::Version.new("5.0")
     expect { Post.follow_assoc(:instance_dependent_tags) }.to raise_error(/instance dependent/)
   end
 end
